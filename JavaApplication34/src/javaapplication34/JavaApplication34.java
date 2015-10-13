f1f3a50a39c45d58552b414681a2ab4cc373d05f
alumno <alumno@aula1-07>
2015-10-13 19:53:45 +0200

    primer ejemplo de java

---
+++ JavaApplication34/src/javaapplication34/JavaApplication34.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication34;
+
+/**
+ *
+ * @author alumno
+ */
+public class JavaApplication34 {
+
+/**
+ *
+ * @author Iván Morán Garcia
+ */
+
+    public static void main(String[] args) {
+        int numero1=30;
+        int numero2;
+        int resultado;
+        numero2=700;
+        resultado=numero1+numero2;
+        System.out.println("El resultado de la suma es: "+resultado);
+        
+    }
+    
+}
