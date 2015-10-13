3feed58d557fb70567cfee588fef1962f5772bc6
efernandezr03 <efernandezr03@aula1-05>
2015-10-13 19:57:32 +0200

    Ejercicio03 Compartido

---
+++ Ej3Compartido/src/ej3compartido/Ej3Compartido.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej3compartido;
+
+/**
+ *
+ * @author efernandezr03
+ */
+public class Ej3Compartido {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+      byte edad=22;
+      float altura = 1.75F;
+      
+      
+        System.out.println("Mi nombre es: Eric Fernández Redondo");
+        System.out.println("Mi edad es : " + edad);
+        System.out.println("Mi altura es : "+ altura+ " metros");
+    }
+    
+}
